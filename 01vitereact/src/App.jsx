fa6a4dbd5e6ccd52d000d41f193780f550e4e138
aryan9855 <aryansinghalagra@gmail.com>
2025-06-03 00:57:22 +0530

    day 1

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import New1 from"./new1"
+function App() {
+  return (
+    <>
+    <New1/>
+    </>
+    
+
+  )
+}
+export default App
---
+++ 01vitereact/src/new1.jsx
@@ -0,0 +1,8 @@
+function New1(){
+    return (
+        <h3> Hello ji   </h3>
+
+    )
+}
+
+export default New1
