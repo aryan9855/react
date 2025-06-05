2a2ba7ee71017d4fe4b4a440b656911540212f4c
aryan9855 <aryansinghalagra@gmail.com>
2025-06-06 01:39:06 +0530

    day 3 bgChanger

---
+++ 04bgChanger/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // add paths where Tailwind classes are used
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
