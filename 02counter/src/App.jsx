0e50e954d23d5b188f7cd888fbe978f15f8ed95f
aryan9855 <aryansinghalagra@gmail.com>
2025-06-06 00:54:52 +0530

    day1 small update

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,8 +8,18 @@ function App() {
 let [counter , setCounter] = useState(0)
 
   const addValue = () =>{
-  setCounter(counter+1)
-  }
+ setCounter(counter+1)
+  //setCounter(counter+1)  interview question : - agar esee hi setCounter ko baar baar call karege to kya hoga??
+  //setCounter(counter+1)   Ans :- kuch nhi hoga counter to ek hi baar add karega chale kitne baar bhi likhdo
+  //setCounter(counter+1)    Reason : ye line number 8 me useState ki vajah se commands jo hai vo ek ek karke nhi           
+  //setCounter(counter+1)       jaegi vo ek bundle banke ek sath jayegi 
+                            // agar real me 4 baar hi inc karna hota to callback use karte
+                            //like this
+  /*setCounter(prevCounter => prevCounter +1)
+  setCounter(prevCounter => prevCounter +1)
+  setCounter(prevCounter => prevCounter +1)
+  setCounter(prevCounter => prevCounter +1)*/
+}
 
   const removeValue = () =>{
     setCounter(counter-1);
