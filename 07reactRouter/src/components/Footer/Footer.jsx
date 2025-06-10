db8b5e5a82ebfa0eaff446596c1c970ebfe7fed9
aryan9855 <aryansinghalagra@gmail.com>
2025-06-11 00:13:00 +0530

    day 7 React Router completed

--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -36,7 +36,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/aryan9855"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
@@ -72,8 +72,8 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        <a href="https://aryan9855.com/" className="hover:underline">
+                            Aryan Singhal
                         </a>
                         . All Rights Reserved.
                     </span>
