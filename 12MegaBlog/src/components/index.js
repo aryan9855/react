b717923d3fbb9ea27caf57f2b76c100026cccdf3
aryan9855 <aryansinghalagra@gmail.com>
2025-07-03 12:38:50 +0530

    day4

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,7 +1,11 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-
+import Container from './container/Container';
+import Logo from './Logo';
+import LogoutBtn from './LogoutBtn/LogoutBtn';
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo
 }
